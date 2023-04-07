27887fa91f72bf3d90cfd018154341054ca4a9f5
仡釩 吳 <jk22145976@gmail.com>
2023-04-07 15:05:20 +0800

    python的連接ok

--- python/main.py
+++ python/main.py
@@ -1,10 +1,21 @@
-from configparser import ConfigParser
 import mysql.connector
 from mysql.connector import Error
+import os
+from configparser import ConfigParser
+from prettytable import PrettyTable
 
-def connect():
+def connect():    
     config=ConfigParser()
-    config.read("../database.config.ini")
+    parent_parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))#abspath main.py的絕對位置
+    config.read(os.path.join(parent_parent_dir,"database.config.ini"))
+
+    ## /database_config.ini
+    # [database]
+    # host=localhost
+    # port=3306
+    # username=
+    # password=
+    # database=customer_order    
 
     db_config={
         'host':config.get('database','host'),
@@ -12,15 +23,38 @@ def connect():
         'user':config.get('database','username'),
         'password':config.get('database','password'),
         'database':config.get('database','database'),
-        'auth_plugin':'cashing_sha2_password'# 在 MySQL 8.0 版本中，預設身份驗證已經變更為 caching_sha2_password
+        # 'auth_plugin':'caching_sha2_password'# 在 MySQL 8.0 版本中，預設身份驗證已經變更為 caching_sha2_password
     }
 
     try:
-        with mysql.connector.connect(**db_config) as conn:
-            print('1')
+        conn=mysql.connector.connect(**db_config) # **db_config是字典 
+        return conn       
     except Error as e:
         print(e)
 
+def sqlData(conn=None,sql="select customers.name,customers.email,orders.order_date,orders.total from customers,orders where customers.customer_id=orders.customer_id;"):
+    cur=conn.cursor()
+    cur.execute(sql)
+    result=cur.fetchall()
+    table=PrettyTable()
+    
+    # 表格的欄位名稱
+    table.field_names = [i[0] for i in cur.description]
+    
+    # 將資料加入表格
+    for row in result:
+        table.add_row(row)
+    print(table)
+
 
 if __name__ == "__main__":
-    connect()
+    conn=connect()
+    # print(conn.is_connected())
+    sql=input('sql指令(預設輸出兩表合併)')
+    if(sql):
+        sqlData(conn,sql)
+    else:
+        sqlData(conn)
+
+    conn.close()
+
